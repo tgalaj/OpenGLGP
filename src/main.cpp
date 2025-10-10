ca0a131d8e362a927df08a30198969a8bcad7bd3
Tomasz Gałaj <tomaszgalaj@gmail.com>
2025-10-10 10:56:56 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -173,6 +173,8 @@ void update()
 void render()
 {
     // OpenGL Rendering code goes here
+    glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
 
 void imgui_begin()
@@ -232,8 +234,6 @@ void imgui_end()
     glfwGetFramebufferSize(window, &display_w, &display_h);
 
     glViewport(0, 0, display_w, display_h);
-    glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-    glClear(GL_COLOR_BUFFER_BIT);
 
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
